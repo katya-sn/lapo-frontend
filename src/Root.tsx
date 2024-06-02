38d5833c88c427c0e8782d437427f58a30f18dd1
katya-sn <kattya.sagun@gmail.com>
2024-06-02 18:55:31 +0300

    deploy2

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,17 @@
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { HomePage } from './pages/HomePage/HomePage';
 import { AboutUsPage } from './pages/AboutUs';
 
 export const Root = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
           <Route path='about' element={<AboutUsPage />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
