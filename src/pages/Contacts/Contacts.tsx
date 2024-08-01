09341605b9c2be0e66bb3b958138ad8f5ff6dbad
katya-sn <kattya.sagun@gmail.com>
2024-08-01 10:13:53 +0300

    some changes

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,9 +1,7 @@
 import { BigSectionsHeader } from "../../components/BigSectionsHeader";
 import { BreadCrumb } from "../../components/BreadCrumb";
 import { BASE_URL } from "../../utils/fetchProducts";
-import {
-  contactsButtons,
-} from "../../utils/socialMedias";
+import { contactsButtons } from "../../utils/socialMedias";
 import "./Contacts.scss";
 
 export const Contacts = () => {
@@ -38,46 +36,61 @@ export const Contacts = () => {
         />
       </div>
 
-      <div className="contacts__carts">
-        <div className="contacts__carts__item">
-          <div className="icon icon-bus" />
-          <p className="contacts__carts__text">
-            424, 376 маршрутка від м. Академістечко
-          </p>
+      <section className="contacts__location">
+        <div className="contacts__carts">
+          <div className="contacts__carts__item">
+            <div className="icon icon-bus" />
+            <p className="contacts__carts__text">
+              424, 376 маршрутка від м. Академістечко
+            </p>
+          </div>
+          <div className="contacts__carts__item">
+            <div className="icon icon-car" />
+            <p className="contacts__carts__text">
+              30 хвилин машиною від м. Академмістечко
+            </p>
+          </div>
         </div>
-        <div className="contacts__carts__item">
-          <div className="icon icon-car" />
-          <p className="contacts__carts__text">
-            30 хвилин машиною від м. Академмістечко
-          </p>
-        </div>
-      </div>
 
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2535.1699471662687!2d30.215195676418347!3d50.54960948021937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472b31bef06fbd91%3A0x8a12336f2cab1314!2z0JbQvtCy0YLQvdC10LLQuNC5INC_0YDQvtCy0YPQu9C-0LosIDQ0LCDQkdGD0YfQsCwg0JrQuNGX0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgMDgyOTI!5e0!3m2!1suk!2sua!4v1717399420901!5m2!1suk!2sua"
-        width="1344"
-        height="455"
-        className="contacts__map"
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2535.1699471662687!2d30.215195676418347!3d50.54960948021937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472b31bef06fbd91%3A0x8a12336f2cab1314!2z0JbQvtCy0YLQvdC10LLQuNC5INC_0YDQvtCy0YPQu9C-0LosIDQ0LCDQkdGD0YfQsCwg0JrQuNGX0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgMDgyOTI!5e0!3m2!1suk!2sua!4v1717399420901!5m2!1suk!2sua"
+          width="1344"
+          height="455"
+          className="contacts__map"
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </section>
 
       <div className="contacts__footer">
-      <div className="contacts__links">
-        {contactsButtons.map((button) => (
-          <div className="contacts__links__item">
-            <a href={button.url} target="_blank" className="contacts__circle" key={button.name}>
-              <div dangerouslySetInnerHTML={{ __html: button.svg }} />
-            </a>
-            <p className="contacts__links__text">{button.text}</p>
-          </div>
-        ))}
+        <div className="contacts__links">
+          {contactsButtons.map((button) => (
+            <div className="contacts__links__item">
+              <a
+                href={button.url}
+                target="_blank"
+                className="contacts__circle"
+                key={button.name}
+              >
+                <div dangerouslySetInnerHTML={{ __html: button.svg }} />
+              </a>
+              <p className="contacts__links__text">{button.text}</p>
+            </div>
+          ))}
         </div>
-        
+
         <div className="contacts__call">
-          <img src={`${BASE_URL}/img/contactsCat.png`} alt="cat" className="contacts__call__cat" />
-          <img src={`${BASE_URL}/img/contactsCatText.png`} alt="cat" className="contacts__call__text" />
+          <img
+            src={`${BASE_URL}/img/contactsCat.png`}
+            alt="cat"
+            className="contacts__call__cat"
+          />
+          <img
+            src={`${BASE_URL}/img/contactsCatText.png`}
+            alt="cat"
+            className="contacts__call__text"
+          />
         </div>
       </div>
     </div>
